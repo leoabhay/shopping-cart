3b6224dd1e4b0db536209ed5058ca1ed5750e7ad
Abhay Chaudhary <abhaycdry10@gmail.com>
2025-02-13 18:32:19 +0545

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+
+                    <p>
+                        &copy;
+                        <a
+                            href="https://abhaychaudhary.com.np"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Abhay Chaudhary
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
